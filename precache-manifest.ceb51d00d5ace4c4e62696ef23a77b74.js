a85ef1e94b4fc45a611ee11afb939ee8a97de705
AarthiKan <67240655+AarthiKan@users.noreply.github.com>
2020-06-21 15:58:05 -0400

    V1.0

---
+++ precache-manifest.ceb51d00d5ace4c4e62696ef23a77b74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a176ee0ca1db4ff416798513f8f5a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e5405d184988e702bd",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc7d730034751cd6cb67",
+    "url": "/static/css/main.d9adf90a.chunk.css"
+  },
+  {
+    "revision": "f9e5405d184988e702bd",
+    "url": "/static/js/2.08291a04.chunk.js"
+  },
+  {
+    "revision": "29fb366f26feaf4efb57e38e52c309b6",
+    "url": "/static/js/2.08291a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7d730034751cd6cb67",
+    "url": "/static/js/main.f3377332.chunk.js"
+  },
+  {
+    "revision": "a9e313254b757c8bf256",
+    "url": "/static/js/runtime-main.2a46a55c.js"
+  },
+  {
+    "revision": "6ac1e80604b7a8903c93436870455ca0",
+    "url": "/static/media/PhoneSamsung.6ac1e806.jpg"
+  },
+  {
+    "revision": "46f1193ae57fd36b04c684c0d3734f11",
+    "url": "/static/media/ShoeKeds.46f1193a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
